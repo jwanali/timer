351d1f61f0d1fe85ac57b49257a27b8196857984
jwanali <ciwan_ali@hotmail.com>
2023-05-19 13:07:24 -0500

    timer

---
+++ timer1.js
@@ -0,0 +1,14 @@
+const input =  process.argv;
+
+for(let i = 2; i < input.length; i++) {
+  
+  if (input[i] < 0 || input[i] === undefined || isNaN(input[i])) {
+
+  } else{
+    setTimeout(() => {
+      process.stdout.write('\x07');
+      console.log(input[i]);
+    }, input[i] * 1000);
+  }
+ 
+}
