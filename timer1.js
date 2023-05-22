59906719c0664aeae0a0b63f308d0c37986e6bba
jwanali <ciwan_ali@hotmail.com>
2023-05-22 15:21:54 -0500

    some updates

--- timer1.js
+++ timer1.js
@@ -2,13 +2,11 @@ const input =  process.argv;
 
 for(let i = 2; i < input.length; i++) {
   
-  if (input[i] < 0 || input[i] === undefined || isNaN(input[i])) {
-
-  } else{
+  if (input[i] > 0 && input[i] !== undefined && !isNaN(input[i])) {
     setTimeout(() => {
       process.stdout.write('\x07');
       console.log(input[i]);
     }, input[i] * 1000);
-  }
+  } 
  
 }
